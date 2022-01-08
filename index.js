39c8385e210cb8271bd054068a2a531f7a50341d
lgndsmobile401 <96932371+lgndsmobile401@users.noreply.github.com>
2022-01-08 09:33:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ var _bulan = 'Rp 22.000,00'
 var _permanen = 'Rp 100.000,00'
 var __permanen = 'Rp 150.000,00'
 
-zeksApikey = 'Alphabott' //ganti pake apikey lu biar limitnya gk cepet abis 
+zeksApikey = 'uOXZNPZ6WJi16953Jc8Q33GBJ8N' //ganti pake apikey lu biar limitnya gk cepet abis 
 ApiZeks = 'https://api.zeks.me' // regis disini klo mau dapat apikeynya
 thumbnail = setting.thumb
 pp_bot = fs.readFileSync(`image/${thumbnail}`)
